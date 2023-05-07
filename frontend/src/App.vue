65311816ac20372b00626fbc6ed6cae88046e7c2
zuramai <saugi.dev@gmail.com>
2023-05-07 19:53:19 +0700

    chore: cleaning up frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,6 @@
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
   <RouterView />
