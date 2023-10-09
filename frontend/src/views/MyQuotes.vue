40220dfcf985375e801f9c05137301d9e26efecb
zuramai <saugi.dev@gmail.com>
2023-10-09 09:00:52 +0700

    feat: show my quotes

--- frontend/src/views/MyQuotes.vue
+++ frontend/src/views/MyQuotes.vue
@@ -1,19 +1,23 @@
 <script lang="ts" setup>
 import Alert from '@/components/Alert.vue';
+import CardQuote from '@/components/CardQuote.vue';
 import MyButton from '@/components/MyButton.vue';
 import Input from '@/components/MyInput.vue';
 import { useStore } from '@/stores';
+import type { Quote } from '@/types';
 import axios from 'axios';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 
-const quotes = ref([])
+const quotes = ref<Quote[]>([])
 const isFetching = ref(true)
 
-axios.get('/quotes')
+
+axios.get('/my-quotes')
     .then(res => {
-        console.log(res)
+        console.log(res.data.data)
+        quotes.value.push(...res.data.data)
         isFetching.value = false 
     }).catch(err => {
         isFetching.value = false 
@@ -28,6 +32,10 @@ axios.get('/quotes')
             </div>
             <p v-if="isFetching">Loading..</p>
             <p v-else-if="!quotes.length">No quote exists. You can <router-link to="/my-quotes/create">add quote</router-link>.</p>
+            
+            <div v-else class="cards grid grid-cols-1 lg:grid-cols-3 gap-5">
+                <CardQuote :quote="quote" v-for="quote in quotes" ></CardQuote>
+            </div>
         </div>
     </main>
 </template>
